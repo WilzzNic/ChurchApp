c0006d457d58490698525ae29fce60f9a7ad36ae
Wilson Nicholas <wilzznic05@gmail.com>
2019-08-24 10:40:20 +0700

    Change Modal Title

--- resources/views/baptis/show.blade.php
+++ resources/views/baptis/show.blade.php
@@ -17,7 +17,7 @@
         <div class="modal-dialog modal-dialog-centered" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                    <h5 class="modal-title" id="exampleModalLabel">Tentukan Waktu</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
